468e94a03880575e12d2fdd558ea4a6cb1f8f1e0
Mochan <mochancrimthann@users.noreply.github.com>
2022-01-30 18:31:07 -0500

    change ecs exports

--- src/arch/ecs.js
+++ src/arch/ecs.js
@@ -1,7 +1,8 @@
 import { ECS } from 'https://esm./run/wolf-ecs'
 
 export const ecs = new ECS()
-export { createEntity, defineComponent } from ecs.bind()
+const { createEntity, defineComponent } = ecs.bind()
+export { createEntity, defineComponent }
 // export const createEntity = ecs.createEntity
 
 export const withComponent = ({ component, properties }, entity) => {

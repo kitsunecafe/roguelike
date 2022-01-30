dcb65c3f85d85ee17c86c6788d006531813fac6a
Mochan <mochancrimthann@users.noreply.github.com>
2022-01-30 18:55:37 -0500

    import position

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { pipe } from 'https://esm.run/rambda'
 import { createEntity, withComponent } from './arch/ecs.js'
+import Position from './components/position.js'
 
 const player = pipe(
-    withComponent(Position)
+    withComponent(Position(1, 1))
 )(createEntity())
 
